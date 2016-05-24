88ec36cb898c60e9e1166eaa4d437058c1e257ae
zengguangyi <m13680538036@163.com>
2016-05-24 23:09:54 +0800

    add WebView

--- app/src/main/java/com/example/zengguangyi/mtuapp/MainActivity.java
+++ app/src/main/java/com/example/zengguangyi/mtuapp/MainActivity.java
@@ -5,20 +5,50 @@ import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnWeather;
+    private TextView text;
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.text);
+        text.setText("123");
+
+        webView = (WebView) findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view,String url){
+                view.loadUrl(url);
+                return true;//不用借助浏览器
+            }
+        });
+        webView.loadUrl("http://www.baidu.com");
 
         btnWeather = (Button)findViewById(R.id.weather);
         /*天气按钮点击事件*/
         btnWeather.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                text.setText("456");
+                /*httpClient*/
+
+
                 AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
                 dialog.setTitle("天气");
                 dialog.setMessage("hahaha");
@@ -33,4 +63,5 @@ public class MainActivity extends AppCompatActivity {
         });
         
     }
+
 }
